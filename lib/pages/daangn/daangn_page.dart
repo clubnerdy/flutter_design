e84c02242c1958e70fee452fd637f62d722bfebb
SeoHoejoeng <seohoejeong@gmail.com>
2025-10-10 17:18:50 +0900

    당근마켓 바로가기 아이콘 추가 및 앱아이콘 추가및 삭제

---
+++ lib/pages/daangn/daangn_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DaangnPage extends StatelessWidget {
+  const DaangnPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
