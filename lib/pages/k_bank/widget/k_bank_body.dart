7e80667da6738344745fa687be9d95eb86b5f945
SeoHoejoeng <seohoejeong@gmail.com>
2025-12-09 20:36:43 +0900

    middle layout end

--- lib/pages/k_bank/widget/k_bank_body.dart
+++ lib/pages/k_bank/widget/k_bank_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_design/pages/k_bank/widget/k_bank_account_list.dart';
+import 'package:flutter_design/pages/k_bank/widget/k_bank_form.dart';
 
 class KBankBody extends StatelessWidget {
   const KBankBody({
@@ -13,6 +14,7 @@ class KBankBody extends StatelessWidget {
       child: Column(
         children: [
           KBankAccountList(),
+          KBankForm(),
         ],
       ),
     );
