f196ef15108a099b635c4d7850224fcb39fe2739
SeoHoejoeng <seohoejeong@gmail.com>
2026-01-20 19:32:36 +0900

    blur appbar end

--- lib/pages/cal_ai/cal_ai_page.dart
+++ lib/pages/cal_ai/cal_ai_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_design/pages/cal_ai/widget/cal_ai_body.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -12,14 +14,45 @@ class CalAiPage extends StatefulWidget {
 
 class _CalAiPageState extends State<CalAiPage> {
   int currentIndex = 0;
+
+  ScrollController _scrollController = ScrollController();
+  double _scrollOffset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _scrollOffset = _scrollController.offset;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF3F7F8),
-      appBar: _appbar(),
-      body: CalAiBody(),
-
-      // 바텀네비게이션 만들기
+      // appBar: _appBar(),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: ListView(
+              controller: _scrollController,
+              padding: EdgeInsets.only(top: 80),
+              children: [
+                CalAiBody(),
+              ],
+            ),
+          ),
+          _buildBlurAppBar(),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print('플로팅액션버튼 눌러짐'),
         backgroundColor: Colors.black,
@@ -27,27 +60,110 @@ class _CalAiPageState extends State<CalAiPage> {
         elevation: 2,
         shape: CircleBorder(),
       ),
-
       floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: _bottomAppBar(),
+    );
+  }
 
-      bottomNavigationBar: BottomAppBar(
-        color: Color(0xFFFFFFFF),
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8,
+  Widget _buildBlurAppBar() {
+    double blurAmount = (_scrollOffset / 100).clamp(0.0, 10.0);
+    double opacity = (_scrollOffset / 100).clamp(0.0, 0.8);
+
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: ClipRect(
         child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          height: 80 + MediaQuery.of(context).padding.top,
+          child: Stack(
             children: [
-              _buildNavItem(
-                FontAwesomeIcons.house,
-                'Home',
-                0,
+              if (_scrollOffset > 0)
+                BackdropFilter(
+                  filter: ImageFilter.blur(
+                    sigmaX: blurAmount,
+                    sigmaY: blurAmount,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.white.withOpacity(opacity * 0.7),
+                          Colors.white.withOpacity(opacity * 0.3),
+                          Colors.white.withOpacity(0),
+                        ],
+                        stops: [0.0, 0.7, 1.0],
+                      ),
+                    ),
+                  ),
+                ),
+              SafeArea(
+                child: Container(
+                  height: 80,
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/calai/logo.svg',
+                        height: 32,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                          decoration: BoxDecoration(
+                            color: Color(0xFFFFFFFF),
+                            borderRadius: BorderRadius.circular(16),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color(0x1A818181),
+                                blurRadius: 10.0,
+                                spreadRadius: 1,
+                                offset: Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            '🔥 1',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              _buildNavItem(FontAwesomeIcons.chartSimple, 'Progress', 1),
-              _buildNavItem(FontAwesomeIcons.gear, 'Settings', 2),
-              SizedBox(width: 1),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  BottomAppBar _bottomAppBar() {
+    return BottomAppBar(
+      color: Color(0xFFFFFFFF),
+      shape: CircularNotchedRectangle(),
+      notchMargin: 8,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _buildNavItem(
+              FontAwesomeIcons.house,
+              'Home',
+              0,
+            ),
+            _buildNavItem(FontAwesomeIcons.chartSimple, 'Progress', 1),
+            _buildNavItem(FontAwesomeIcons.gear, 'Settings', 2),
+            SizedBox(width: 1),
+          ],
         ),
       ),
     );
@@ -72,47 +188,6 @@ class _CalAiPageState extends State<CalAiPage> {
           ),
         ],
       ),
-    );
-  }
-
-  AppBar _appbar() {
-    return AppBar(
-      title: SvgPicture.asset(
-        'assets/calai/logo.svg',
-        height: 32,
-      ),
-      backgroundColor: Color(0xFFF3F7F8),
-      automaticallyImplyLeading: false,
-      toolbarHeight: 80,
-      actionsPadding: EdgeInsets.only(right: 16),
-      actions: [
-        InkWell(
-          onTap: () {},
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-            decoration: BoxDecoration(
-              color: Color(0xFFFFFFFF),
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0x1A818181),
-                  blurRadius: 10.0,
-                  spreadRadius: 1,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Row(
-              children: [
-                Text(
-                  '🔥 1',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
